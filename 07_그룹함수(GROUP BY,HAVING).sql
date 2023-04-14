918fe09233398a42404bb6a10caf3f2781585517
Leejunho <wpsxmfwnsgh@gmail.com>
2023-04-14 15:31:14 +0900

    sql

--- 07_그룹함수(GROUP BY,HAVING).sql
+++ 07_그룹함수(GROUP BY,HAVING).sql
@@ -63,18 +63,83 @@ GROUP BY job_id
 HAVING COUNT(*) >= 5;
 
 --부서 아이디가 50이상인 것들은 그룹화 시키고, 그룹 월급 평균이 5000 이상만 조회
-SELECT
+SELECT  5 
     department_id,
     AVG(salary) AS 평균
-FROM employees -- 모든 요소
-WHERE department_id >= 50 --조회 조건
-GROUP BY department_id -- 그룹화
-HAVING AVG(salary) >= 5000 -- 그룹화 조건
-ORDER BY department_id DESC; --정렬
+FROM employees -- 모든 요소 1
+WHERE department_id >= 50 --조회 조건 2 
+GROUP BY department_id -- 그룹화 3
+HAVING AVG(salary) >= 5000 -- 그룹화 조건 4
+ORDER BY department_id DESC; --정렬  6
+
+
+/*
+문제 1.
+사원 테이블에서 JOB_ID별 사원 수를 구하세요.
+사원 테이블에서 JOB_ID별 월급의 평균을 구하세요. 월급의 평균 순으로 내림차순 정렬하세요
+*/
+
+SELECT
+    job_id,
+    count(*) AS 사원수
+FROM employees 
+GROUP BY job_id;
+                                                                      
+
+SELECT
+    job_id,
+    AVG(salary) AS 평균월급
+FROM employees
+GROUP BY 평균월급;
+
+/*
+문제 2.
+사원 테이블에서 입사 년도 별 사원 수를 구하세요.
+(TO_CHAR() 함수를 사용해서 연도만 변환합니다. 그리고 그것을 그룹화 합니다.)
+*/
+
+SELECT
+    TO_CHAR(hrie_date, 'YY'),
+    COUNT(*)
+FROM employees
+GROUP BY TO_CHAR(hire_date, 'YY') 
+
+
+
+/*
+문제 3.
+급여가 5000 이상인 사원들의 부서별 평균 급여를 출력하세요. 
+단 부서 평균 급여가 7000이상인 부서만 출력하세요.
+*/
+
+SELECT
+    department_id,
+    AVG(salary) AS 평균급여
+FROM employees
+WHERE salary >= 5000
+GROUP BY department_id
+HAVING AVG(salary) >= 7000;
 
 
 
 
+/*
+문제 4.
+사원 테이블에서 commission_pct(커미션) 컬럼이 null이 아닌 사람들의
+department_id(부서별) salary(월급)의 평균, 합계, count를 구합니다.
+조건 1) 월급의 평균은 커미션을 적용시킨 월급입니다.
+조건 2) 평균은 소수 2째 자리에서 절삭 하세요
+
+*/
+
+SELECT
+    department_id,
+    TRUNC (AVG(salary + (salary*commission_pct)),2),
+    SUM(salary + salary*commission_pct),
+    COUNT(*)
+FROM employees
+WHERE commission_pct IS NOT NULL 
+GROUP BY department_id;
 
 
 
